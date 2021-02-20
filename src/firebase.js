38e4d7b6e10806252e9e51ef51fa40c0e5d7143e
aleksssaints <79362981+aleksssaints@users.noreply.github.com>
2021-02-20 19:30:52 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,6 @@ import 'firebase/database'
 import 'firebase/auth'
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyCeSmkaeRa5IgWWLMbDpVzt4Qr8UCzTiN0',
-  authDomain: 'weightmonitoring-80981.firebaseapp.com',
-  databaseURL: 'https://weightmonitoring-80981-default-rtdb.firebaseio.com',
-  projectId: 'weightmonitoring-80981',
-  storageBucket: 'weightmonitoring-80981.appspot.com',
-  messagingSenderId: '621739309399',
-  appId: '1:621739309399:web:690a64317a28a39bed6612'
 }
 firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
