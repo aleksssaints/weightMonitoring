8364cd42d5fed483e1d8611e3f9128e9aa81c444
mihailalekseev <MkiS1KbImz>
2021-02-20 03:17:50 +0300

    Changes in graph

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -48,27 +48,32 @@ export default {
       this.weightList = s.val()
     })
 
-    function renderChart(data, labels) {
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart =  ChartJSImage()(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'This week',
-                data: data,
-            }]
-          },
-      });
-    }
+  var lineChart = new Chart(myChart, {
+    type: 'line',
+    data: speedData,
+    options: chartOptions
+  });
 
-  $("#renderBtn").click(
-    function () {
-        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
-        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-        renderChart(data, labels);
+  var speedData = {
+  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+  datasets: [{
+      label: "Car Speed",
+      data: [0, 59, 75, 20, 20, 55, 40],
+    }]
+  };
+ 
+  var chartOptions = {
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        boxWidth: 80,
+        fontColor: 'black'
+      }
     }
-  );
+  };
+
+
 
   }
 }
