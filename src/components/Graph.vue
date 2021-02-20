968d8396717a17139d552e787f39394867300fb1
mihailalekseev <MkiS1KbImz>
2021-02-20 03:09:32 +0300

    Added failed charg.js code

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,17 +1,28 @@
 <template>
     <div>
         <h1>График изменения веса</h1>
+
         <ul>
             <li v-for="weight of weightList" :key="weight['.key']">
                 <p>{{weight}}</p>
             </li>
         </ul>
+
+        <button id="renderBtn">
+          Render
+        </button>
+
+        <div class="container">
+          <canvas id="myChart" width="600" height="400"></canvas>
+        </div>
+
         <h2><router-link to="/editing">Добавить вес</router-link></h2>
     </div>
 </template>
 
 <script>
 import { weightRef } from '../firebase'
+import ChartJSImage from 'chart.js-image';
 
 export default {
   name: 'graph',
@@ -36,6 +47,29 @@ export default {
     weightRef.on('value', (s) => {
       this.weightList = s.val()
     })
+
+    function renderChart(data, labels) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart =  ChartJSImage()(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'This week',
+                data: data,
+            }]
+          },
+      });
+    }
+
+  $("#renderBtn").click(
+    function () {
+        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+        renderChart(data, labels);
+    }
+  );
+
   }
 }
 </script>
